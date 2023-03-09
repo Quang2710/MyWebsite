3d81041b7085e465178d823aa01693685ec8fef8
QuangVN <quangtb2k1@gmail.com>
2023-03-09 15:52:03 +0700

    create LoginForm

--- angular/my-website-app/src/app/app-routing.module.ts
+++ angular/my-website-app/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'app',
+        component: AppComponent,
+        children: [
+            {
+               path: 'login', component: LoginComponent,
+
+            },
+            {
+                path: '**', redirectTo: 'notifications'
+            }
+        ]
+    }
+    ])
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
